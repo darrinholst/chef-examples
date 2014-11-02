751619980e47033a05273bb1ff8faa49ad7b571c
Darrin Holst <darrinholst@gmail.com>
2014-11-02 13:29:31 -0600

    switch from definition to lwrp

--- rails_app/vendor/cookbooks/rails_app/metadata.rb
+++ rails_app/vendor/cookbooks/rails_app/metadata.rb
@@ -1,6 +1,7 @@
-description "Rails Apps"
-version     "0.0.1"
-depends     "monit"
-depends     "nginx"
-depends     "postgresql"
+description "deploys a rails app"
+version     '0.0.1'
+depends     'monit'
+depends     'nginx'
+depends     'postgresql'
+depends     'ruby_build'
 
---
+++ rails_app/vendor/cookbooks/rails_app/resources/default.rb
@@ -0,0 +1,13 @@
+actions :deploy
+default_action :deploy
+
+attribute :id,                    :kind_of => String, :name_attribute => true, :required => true
+attribute :environment,           :kind_of => String, :required => true
+attribute :repository,            :kind_of => String, :required => true
+attribute :revision,              :kind_of => String, :required => true
+attribute :ruby_version,          :kind_of => String, :required => true
+attribute :ssl_enabled,           :kind_of => [TrueClass, FalseClass]
+attribute :delayed_job,           :kind_of => [TrueClass, FalseClass]
+attribute :server_names,          :kind_of => Array, :required => true
+attribute :environment_variables, :kind_of => Hash, :required => true
+
